a732e1ef6c13b15dc5bc47d435d3b59b74727e2b
LEO-Malay <malaybhavsar.290@gmail.com>
2021-12-28 23:28:29 +0530

    Working on new design of Item Page

--- src/views/Item.js
+++ src/views/Item.js
@@ -1,13 +1,5 @@
 import React, {useState} from 'react';
-import {
-  ActivityIndicator,
-  View,
-  Image,
-  Text,
-  StyleSheet,
-  Dimensions,
-  TextInput,
-} from 'react-native';
+import {ActivityIndicator, View, Image, Text, TextInput} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 // Helper Component
 import {ScreenHeader} from '../components/Header';
@@ -22,33 +14,6 @@ import SUB from '../assets/img/products/sub.jpg';
 // Style
 import style from '../Style/style';
 import {Black, White, Yellow} from '../Style/color';
-const Localstyle = StyleSheet.create({
-  SubContainer: {
-    flex: 1,
-    paddingTop: 30,
-    paddingHorizontal: 20,
-    backgroundColor: Black,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-  },
-  Img: {
-    width: Dimensions.get('window').width,
-    resizeMode: 'contain',
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    alignSelf: 'auto',
-    resizeMode: 'cover',
-  },
-  Gen: {paddingVertical: 5, paddingHorizontal: 10},
-  TextIn: {
-    backgroundColor: White,
-    borderRadius: 10,
-    width: Dimensions.get('window').width * 0.8,
-    height: 100,
-    padding: 5,
-    margin: 5,
-  },
-});
 // Request
 import {AddCart} from '../redux/Actions/Cart';
 
@@ -66,103 +31,117 @@ const Item = ({navigation}) => {
     else return '';
   };
   return (
-    <View style={style.Container}>
+    <View>
       <ScreenHeader navigation={navigation} props={{name: 'Item'}} />
-      <Image source={getImg(product?.category)} style={Localstyle.Img} />
-      <View style={Localstyle.SubContainer}>
-        <View style={style.Inline}>
-          <Text
-            style={[
-              style.Text,
-              style.TextWhite,
-              style.Subtitle,
-              Localstyle.Gen,
-              {flex: 1},
-            ]}>
-            {product?.name}
-          </Text>
-          <Text
-            style={[
-              style.Text,
-              style.TextWhite,
-              style.Desc,
-              style.Center,
-              Localstyle.Gen,
-            ]}>
-            ${product?.price}
-          </Text>
-        </View>
-        <Text style={[style.Text, style.TextWhite, style.Desc, Localstyle.Gen]}>
-          Ingredients:
-          {' ' + (product?.ingredients && product?.ingredients.join(', '))}
-          {'\n'}
-          {'\n'}
-          Any special request? (*Terms {'&'} Conditions Apply)
-        </Text>
-        <TextInput style={Localstyle.TextIn} multiline={true} />
-        <View style={[style.Inline, {paddingTop: 15}]}>
-          <Text
-            style={[style.Text, style.TextWhite, style.Desc, Localstyle.Gen]}>
-            Quantity:{'\t'}
-          </Text>
-          <View
-            style={[
-              {
-                backgroundColor: Yellow,
-                borderRadius: 50,
-                width: 95,
-              },
-              style.Inline,
-            ]}>
-            <IconButton
-              props={{
-                name: 'remove',
-                size: 15,
-                color: Black,
-                onPress: () => {
-                  if (qty > 1) setQty(qty - 1);
-                },
-              }}
-              style={[style.Left]}
-            />
-            <Text
-              style={[
-                style.Text,
-                style.Center,
-                {flex: 1, color: Black, textAlign: 'center'},
-              ]}>
-              {qty}
-            </Text>
-            <IconButton
-              props={{
-                name: 'add',
-                size: 15,
-                color: Black,
-                onPress: () => {
-                  if (product.buyQtyLimit > qty) setQty(qty + 1);
-                },
-              }}
-              style={style.Right}
-            />
-          </View>
-        </View>
-        <View style={style.screenBottom}>
-          <Button
+      <Text
+        style={{
+          fontSize: 35,
+          fontWeight: 'bold',
+          textAlign: 'center',
+          paddingVertical: 10,
+        }}>
+        {product?.name}
+      </Text>
+      <Image
+        source={getImg(product?.category)}
+        style={{
+          width: '100%',
+          resizeMode: 'contain',
+        }}
+      />
+      <Text
+        style={{
+          fontSize: 25,
+          fontWeight: 'bold',
+          textAlign: 'center',
+          paddingVertical: 10,
+        }}>
+        ${product?.price}
+      </Text>
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <View
+          style={{
+            backgroundColor: Yellow,
+            borderRadius: 50,
+            width: 140,
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+          }}>
+          <IconButton
             props={{
-              text: isLoading ? (
-                <ActivityIndicator size="small" color="#0000ff" />
-              ) : (
-                'Add to Cart [Total: $' + qty * product.price + ']'
-              ),
-              width: 300,
+              name: 'remove',
+              size: 15,
               color: Black,
-              bgcolor: Yellow,
-              onPress: async () => {
-                await dispatch(AddCart(product._id, qty));
+              onPress: () => {
+                if (qty > 1) setQty(qty - 1);
+              },
+            }}
+          />
+          <Text
+            style={{
+              fontSize: 15,
+              fontWeight: 'bold',
+              paddingTop: 12,
+              paddingHorizontal: 13,
+            }}>
+            {qty}
+          </Text>
+          <IconButton
+            props={{
+              name: 'add',
+              size: 15,
+              color: Black,
+              onPress: () => {
+                if (product.buyQtyLimit > qty) setQty(qty + 1);
               },
             }}
           />
         </View>
+      </View>
+      <Text
+        style={{
+          fontSize: 15,
+          fontWeight: 'bold',
+          textAlign: 'center',
+          paddingVertical: 10,
+        }}>
+        Ingredients:
+        {' ' + (product?.ingredients && product?.ingredients.join(', '))}
+        {'\n'}
+        {'\n'}
+        Any special request? (*Terms {'&'} Conditions Apply)
+      </Text>
+      <TextInput
+        style={{
+          backgroundColor: White,
+          borderRadius: 10,
+          width: 200,
+          height: 100,
+          padding: 5,
+          margin: 5,
+        }}
+        multiline={true}
+      />
+      <View style={style.screenBottom}>
+        <Button
+          props={{
+            text: isLoading ? (
+              <ActivityIndicator size="small" color="#0000ff" />
+            ) : (
+              'Add to Cart [Total: $' + qty * product.price + ']'
+            ),
+            width: 300,
+            color: Black,
+            bgcolor: Yellow,
+            onPress: async () => {
+              await dispatch(AddCart(product._id, qty));
+            },
+          }}
+        />
       </View>
       <CartErrorHandler />
     </View>
